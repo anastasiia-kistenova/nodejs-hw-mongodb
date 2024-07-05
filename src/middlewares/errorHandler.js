b3a510110f63c2e9ea918346e32eb4a694df9a95
anastasiia-kistenova <143128667+anastasiia-kistenova@users.noreply.github.com>
2024-07-05 12:53:24 +0300

    hw-node-3(1)

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,18 +1,20 @@
-import { HttpError } from 'http-errors';
 
-export const errorHandler = (err, req, res) => {
-  if (err instanceof HttpError) {
+import createHttpError from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof createHttpError.HttpError) {
     res.status(err.status).json({
       status: err.status,
-      message: err.name,
-      data: err,
+      message: err.message,
+      errors: err.errors || [],
     });
+    next();
     return;
   }
 
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
-    data: err.message,
+    errors: err.message,
   });
 };
